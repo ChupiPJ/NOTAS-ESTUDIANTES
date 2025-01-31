5b1aacd3c1b1f9b5f43e17b9b4532a38fd0c83ba
ChupiJP <hectormargaryan11@gmail.com>
2025-01-31 19:11:09 +0400

    feat: :sparkles: Termine mas funciones
    Termine las funciones para calcular la nota definitiva, para devolver los parciales de todos los estudiantes para el promedio parcial y mas.

--- data_processing.py
+++ data_processing.py
@@ -24,8 +24,39 @@ def validar_notas(df):
     else:
         print("✅ Todos los parciales tienen valores válidos.")
 
-    return df  # Retornar el DataFrame corregido
+    return df
+
+def calcular_notas_definitivas(df):
+    df["NotaDefinitiva"] = (df["Parcial1"] + df["Parcial2"]) / 2
+    return df
+
+
+def ParcialesTodosEstudiantes(df):
+    print("\nNotas Definitivas de los Estudiantes:")
+    print(df[["Nombre", "NotaDefinitiva"]])
+
+def PromedioParcial(df, parcial):
+    if parcial not in [1, 2]:
+        print(f"Parcial {parcial} no válido. Debe ser 1 o 2.")
+        return
+    
+    promedio = df[f"Nota_Parcial_{parcial}"].mean()
+    print(f"\nPromedio del Parcial {parcial}: {promedio:.2f}")
+
+def resultados(df):
+    hombres = df[df["Sexo"] == "M"]
+    mujeres = df[df["Sexo"] == "F"]
+
+    prom_hombres = hombres["NotaDefinitiva"].mean() if not hombres.empty else 0
+    prom_mujeres = mujeres["NotaDefinitiva"].mean() if not mujeres.empty else 0
+    reprobados = df[df["NotaDefinitiva"] < APROBADO].shape[0]
+
+    print("\nResultados:")
+    print(f"Promedio de Notas Definitivas de Hombres: {prom_hombres:.2f}")
+    print(f"Promedio de Notas Definitivas de Mujeres: {prom_mujeres:.2f}")
+    print(f"Número de Estudiantes Reprobados: {reprobados}")
+
+def resultados(df):
 
 # Ejecutar validación
 df = pd.read_csv(CSV_FILENAME)
-validar_notas(df)
